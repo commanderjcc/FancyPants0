de60de90da333f9519d60d5dbcc81c3ebc07e0f5
Joshua Christensen <jj1355810@gmail.com>
2019-05-18 13:08:36 -0400

    Add Bass classes

--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,37 @@
 ﻿Public Class Form1
 
+    Class Game
+
+    End Class
+    Class viewManager
+
+
+
+
+    End Class
+
+    Class dataManager
+
+    End Class
+
+    Class gameManager
+
+    End Class
+
+    Class Tile
+
+    End Class
+
+    Class Player
+
+    End Class
+
+    Class entity
+
+    End Class
+
+
 End Class
+
+
+
